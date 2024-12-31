44acc7d1673477d25dc032db9d6df4659d7343c3
enaskhann <enaskhan@hotmail.com>
2024-12-31 15:05:16 +0300

    #codeforbatches

--- app/Http/Controllers/BatchController.php
+++ app/Http/Controllers/BatchController.php
@@ -35,32 +35,34 @@ class BatchController extends Controller
             
         }
     
-
     public function createbtches()
         {   
             $vl = VirtualLocation::all();
              foreach ($vl as $vl) {
              $virtualpilgrims = (int)($vl->pilgrims / 188); 
                  for ($i = 1; $i <= $virtualpilgrims; $i++) {
-                    $officeIDcode = VirtualLocation::where('id', $vl->id)->first()?->office_id ?? null;
+                    $officeID = VirtualLocation::where('id', $vl->id)->first()?->office_id ?? null;
+                    $companyID = VirtualLocation::where('id', $vl->id)->first()?->company_id ?? null;
                     $companyIDcode = Company::where('id', $vl->company_id)->first()?->code ?? null;
+                    $num = $i;
                  Batch::create([
                          'virtual_location_id' => $vl->id,
                          'pilgrims'=> 188 ,
-                         'code'=> $officeIDcode . ' ' .$companyIDcode,
+                         'code'=> $companyIDcode.''.sprintf('%04d',$companyID).'' .sprintf('%04d',$officeID). '' .sprintf('%04d',$num),
                      ]);}
                  
             $remainPilgrims = $vl->pilgrims % 188;
             if($remainPilgrims > 0 && $remainPilgrims >12){
+                $num +=1;
                 Batch::create([
                             'virtual_location_id' => $vl->id,
                             'pilgrims' => $remainPilgrims,
-                            'code' => 1,
-                                 ]);
+                            'code'=> $companyIDcode.''.sprintf('%04d',$companyID).'' .sprintf('%04d',$officeID). '' .sprintf('%04d',$num),
+                        ]);
             }
             elseif ($remainPilgrims> 0 && $remainPilgrims<= 12){
             $lastBatch = Batch::where('virtual_location_id', $vl->id)->orderBy('id', 'desc')->first(); 
-            $lastBatch->pilgrims = $remainPilgrims;
+            $lastBatch->pilgrims += $remainPilgrims;
             $lastBatch->save();
            }
             }
